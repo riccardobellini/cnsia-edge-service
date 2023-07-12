9e695595e3a782f48869527b1315ede000565cb1
Riccardo Bellini <riccardo.bellini@rold.com>
2023-07-12 17:56:23 +0200

    User controller test

---
+++ src/test/java/com/polarbookshop/edgeservice/controller/UserControllerTest.java
@@ -0,0 +1,62 @@
+package com.polarbookshop.edgeservice.controller;
+
+import com.polarbookshop.edgeservice.config.SecurityConfig;
+import com.polarbookshop.edgeservice.model.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.reactive.server.WebTestClientConfigurer;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@WebFluxTest(UserController.class)
+@Import(SecurityConfig.class)
+class UserControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    @Test
+    void whenNotAuthenticatedThenHttp401() {
+        webTestClient.get()
+                .uri("/user")
+                .exchange()
+                .expectStatus().isUnauthorized();
+    }
+
+    @Test
+    void whenAuthenticatedThenReturnUser() {
+        final User expectedUser = new User("jon.snow",
+                "Jon",
+                "Snow",
+                List.of("employee", "customer"));
+
+        webTestClient
+                .mutateWith(configureMockOidcLogin(expectedUser))
+                .get()
+                .uri("/user")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody(User.class)
+                .value(user -> assertThat(user).isEqualTo(expectedUser));
+    }
+
+    private WebTestClientConfigurer configureMockOidcLogin(User user) {
+        return SecurityMockServerConfigurers.mockOidcLogin().idToken(builder -> {
+            builder.claim(StandardClaimNames.PREFERRED_USERNAME, user.username())
+                    .claim(StandardClaimNames.GIVEN_NAME, user.firstName())
+                    .claim(StandardClaimNames.FAMILY_NAME, user.lastName());
+        });
+    }
+}
