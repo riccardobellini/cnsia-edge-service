c54b7f0fc3f3512e6e478f2cde63f79eed54972d
Riccardo Bellini <riccardo.bellini@rold.com>
2023-07-12 15:34:59 +0200

    Endpoint to retrieve user information

---
+++ src/main/java/com/polarbookshop/edgeservice/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.polarbookshop.edgeservice.controller;
+
+import com.polarbookshop.edgeservice.model.User;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @GetMapping("user")
+    public Mono<User> getUser(@AuthenticationPrincipal OidcUser oidcUser) {
+        final var user = new User(oidcUser.getPreferredUsername(),
+                oidcUser.getGivenName(),
+                oidcUser.getFamilyName(),
+                List.of("employee", "customer"));
+        return Mono.just(user);
+    }
+}
